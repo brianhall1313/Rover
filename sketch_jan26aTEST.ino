e4ce195498b9fd50a40cce772f84194cf5131f2f
Brian <brianhall1313@gmail.com>
2025-01-28 15:19:23 -0800

    ultrasonics set up, currently in cm

--- sketch_jan26aTEST.ino
+++ sketch_jan26aTEST.ino
@@ -1,5 +1,6 @@
 #include <Servo.h>
 
+//head servo
 Servo myServo;
 #define HEAD 3
 //max 180
@@ -9,6 +10,11 @@ int centerAngle = 90;
 //min 0
 int rightAngle = 45;
 
+//Ultra Sonic sensor
+int Echo = A4;  
+int Trig = A5; 
+
+//wheel vars
 #define ENA 5
 #define ENB 6
 #define IN1 7
@@ -17,27 +23,6 @@ int rightAngle = 45;
 #define IN4 11
 
 int carSpeed = 250;
-
-void setup() {
-  // wheel stuff:
-  pinMode(HEAD, OUTPUT);
-  pinMode(IN1,OUTPUT);
-  pinMode(IN2,OUTPUT);
-  pinMode(IN3,OUTPUT);
-  pinMode(IN4,OUTPUT);
-  pinMode(ENA,OUTPUT);
-  pinMode(ENB,OUTPUT);
-  analogWrite(ENA,carSpeed);
-  analogWrite(ENB,carSpeed);
-  stop();
-  //head stuff
-  myServo.attach(3);
-  myServo.write(centerAngle);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly
-}
 
 void adjustSpeed(int newSpeed) {
   carSpeed = newSpeed;
@@ -88,4 +73,41 @@ void stop(){
   digitalWrite(IN3,LOW);
   digitalWrite(IN4,LOW);
 
+}
+
+int distanceTest() {
+  digitalWrite(Trig, LOW);   
+  delayMicroseconds(2);
+  digitalWrite(Trig, HIGH);  
+  delayMicroseconds(20);
+  digitalWrite(Trig, LOW);   
+  float Fdistance = pulseIn(Echo, HIGH);  
+  Fdistance= Fdistance / 58;       
+  return (int)Fdistance;
+}
+
+void setup() {
+  // wheel stuff:
+  pinMode(HEAD, OUTPUT);     
+  pinMode(Echo, INPUT);    
+  pinMode(Trig, OUTPUT);  
+  pinMode(IN1,OUTPUT);
+  pinMode(IN2,OUTPUT);
+  pinMode(IN3,OUTPUT);
+  pinMode(IN4,OUTPUT);
+  pinMode(ENA,OUTPUT);
+  pinMode(ENB,OUTPUT);
+  analogWrite(ENA,carSpeed);
+  analogWrite(ENB,carSpeed);
+  stop();
+  //head stuff
+  myServo.attach(3);
+  myServo.write(centerAngle);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly
+  delay(3000);
+  Serial.println(distanceTest());
 }
